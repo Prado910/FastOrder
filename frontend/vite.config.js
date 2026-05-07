209b5d32ecd38366a1046251966431a7cd71b0c6
Juan Prado <juanandrespradobo06@gmail.com>
2026-05-07 09:59:01 -0500

    render host allowed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['fastorder-1-dook.onrender.com', 'localhost'],
     host: '0.0.0.0',
     proxy: {
       '/api': {
