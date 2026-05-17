3d8fc0c1e469a822798796af8c23cce1a8e2b5a6
Juan Prado <juanandrespradobo06@gmail.com>
2026-05-17 12:25:01 -0500

    host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    allowedHosts: true,
     host: '0.0.0.0',
     proxy: {
       '/api': {
